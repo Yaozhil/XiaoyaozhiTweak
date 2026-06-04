4c42fcc4145447ea58c3c7595c9564a5fed9c730
Rouneed <rouneed@users.noreply.github.com>
2026-06-04 13:01:18 +0800

    Fix reward view import

--- UI/YZGlassSheetController.m
+++ UI/YZGlassSheetController.m
@@ -6,6 +6,7 @@
 #import "YZConfigManager.h"
 #import "YZPluginLifecycle.h"
 #import "YZCrashGuard.h"
+#import "YZRewardView.h"
 
 #import <AudioToolbox/AudioToolbox.h>
 #import <QuartzCore/QuartzCore.h>
